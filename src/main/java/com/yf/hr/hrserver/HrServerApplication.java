57281577be0ea93b3db19b186c82f4d630e8dc11
yf <Yf040713@icloud.com>
2025-12-28 16:25:41 +0800

    initial commit: hr-server

---
+++ src/main/java/com/yf/hr/hrserver/HrServerApplication.java
@@ -0,0 +1,15 @@
+package com.yf.hr.hrserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+//启动类
+@SpringBootApplication
+public class HrServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yf/hr/hrserver/HrServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yf.hr.hrserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
